28dfe1d1b8e77957c42b7b1cd4b5e7ff4aec9ad5
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2022-10-31 17:57:44 +0900

    add/skeleton

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -13,7 +13,7 @@ viewsRouter.use("/store", serveStatic("store"));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use("/", serveStatic(""));
-
+``;
 // views폴더 내의 ${resource} 폴더 내의 모든 파일을 웹에 띄우며,
 // 이 때 ${resource}.html 을 기본 파일로 설정함.
 function serveStatic(resource) {
