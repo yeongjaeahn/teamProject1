76263804b2d6811c018bdbfb84ad0f642ef388b7
yeongjae <yeongjae9501@gmail.com>
2022-11-05 10:49:22 +0900

    shop lode item api

---
+++ src/views/shop/shop.js
@@ -0,0 +1,28 @@
+let itemList = document.querySelector(".item-list");
+
+insertProductElement();
+
+async function insertProductElement() {
+  const res = await fetch(`localhost:5000/itemlist`);
+  const products = await res.json();
+
+  products.forEach((product) => {
+    const title = product.title;
+    const price = product.price;
+    const image = product.img;
+
+    itemList.insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="product">
+            <div class="sumnail">
+              <img src="${image}" alt="clothes-image" />
+            </div>
+            <div class="product-info">
+              <p class="name">${title}</p>
+              <p class="price">${price} 원</p>
+            </div>
+    `
+    );
+  });
+}
