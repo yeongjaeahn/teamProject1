e0969d1801953cb1878ca628fbc5ba8905a1d49e
yeongjae <yeongjae9501@gmail.com>
2022-11-05 15:15:39 +0900

    item lode api fix

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,7 +1,7 @@
 // navibar 스크롤시 색상변경
 
 window.addEventListener("scroll", function () {
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
 });
 var $header = $("header"); //헤더를 변수에 넣기
 var $header1 = $("header a"); //헤더 글씨
@@ -21,34 +21,37 @@ $window.on("scroll", function () {
 });
 
 // section 2 상품 목록 가져오기
+let item;
+let html = "";
+async function getData() {
+  try {
+    let response = await fetch("/api/itemlist");
+    item = await response.json();
+    console.log(item);
+  } catch (err) {
+    console.log(err);
+  }
 
-let itemList = document.querySelector(".item-list");
-
-insertProductElement();
-
-async function insertProductElement() {
-  const res = await fetch(`localhost:5000/itemlist`);
-  const products = await res.json();
-
-  products.forEach((product) => {
-    const title = product.title;
-    const price = product.price;
-    const image = product.img;
-
-    itemList.insertAdjacentHTML(
-      "beforeend",
-      `
-    <div class="product">
+  let itemList = document.querySelector(".item-list");
+  for (let i = 0; i < 9; i++) {
+    html += `
+          <div class="product">
             <div class="sumnail">
-              <img src="${image}" alt="clothes-image" />
+              <img src="img/b6ce11f84cd989d835461e5736a5e503.jpg" alt="" />
             </div>
             <div class="product-info">
-              <p class="name">${title}</p>
-              <p class="price">${price} 원</p>
+              <p class="name">${item[i].name}</p>
+              <p class="price">${item[i].price}</p>
             </div>
-    `
-    );
-  });
+          </div>
+        `;
+  }
+
+  // document.querySelector("#first-name").innerHTML = itemName;
+  // document.querySelector(".price").innerHTML = itemPrice;
+  itemList.innerHTML = html;
 }
+
+getData();
 
 // sec
--- src/views/shop/shop.js
+++ src/views/shop/shop.js
@@ -1,30 +1,32 @@
-// load item list
+let item;
+let html = "";
+async function getData() {
+  try {
+    let response = await fetch("/api/itemlist");
+    item = await response.json();
+    console.log(item);
+  } catch (err) {
+    console.log(err);
+  }
 
-let itemList = document.querySelector(".item-list");
+  let itemList = document.querySelector(".item-list");
+  for (let i = 0; i < item.length; i++) {
+    html += `
+      <div class="product">
+        <div class="sumnail">
+          <img src="img/b6ce11f84cd989d835461e5736a5e503.jpg" alt="" />
+        </div>
+        <div class="product-info">
+          <p class="name">${item[i].name}</p>
+          <p class="price">${item[i].price}</p>
+        </div>
+      </div>
+    `;
+  }
 
-insertProductElement();
-
-async function insertProductElement() {
-  const res = await fetch(`localhost:5000/itemlist`);
-  const products = await res.json();
-
-  products.forEach((product) => {
-    const title = product.title;
-    const price = product.price;
-    const image = product.img;
-
-    itemList.insertAdjacentHTML(
-      "beforeend",
-      `
-    <div class="product">
-            <div class="sumnail">
-              <img src="${image}" alt="clothes-image" />
-            </div>
-            <div class="product-info">
-              <p class="name">${title}</p>
-              <p class="price">${price} 원</p>
-            </div>
-    `
-    );
-  });
+  // document.querySelector("#first-name").innerHTML = itemName;
+  // document.querySelector(".price").innerHTML = itemPrice;
+  itemList.innerHTML = html;
 }
+
+getData();
