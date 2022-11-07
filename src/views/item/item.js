d0e0f6fbf42ae0814b541dd8dd00ed0e876cc51e
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2022-11-07 11:09:41 +0900

    add/item-detail

--- src/views/item/item.js
+++ src/views/item/item.js
@@ -1,4 +1,6 @@
 let baketBtn = document.querySelector("#baket");
+const itemDetailName = document.querySelector(".name");
+const itemDetailPrice = document.querySelector(".price");
 
 baketBtn.addEventListener("click", function () {
   if (confirm("장바구니로 넘어가시겠습니까?")) {
@@ -8,3 +10,27 @@ baketBtn.addEventListener("click", function () {
 
   localStorage.setItem();
 });
+
+let item;
+let name = "";
+const id = window.location.pathname;
+const _id = id.slice(6, id.length - 1);
+console.log(_id);
+async function getDetailData() {
+  try {
+    let response = await fetch(`/api/item/${_id}`);
+    item = await response.json();
+    console.log(item);
+
+    const itemName = item.name;
+    const itemPrice = item.price;
+
+    console.log(itemName, itemPrice);
+
+    itemDetailName.innerHTML = itemName;
+    itemDetailPrice.innerHTML = itemPrice;
+  } catch (err) {
+    console.log(err);
+  }
+}
+getDetailData();
